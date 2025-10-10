071f8bbac53e7e44c959b1035c8505735a1dba1e
melissa-azeb <156686935+melissa-azeb@users.noreply.github.com>
2025-10-10 14:17:20 +0200

    Update calculator.h

--- include/calculator.h
+++ include/calculator.h
@@ -2,3 +2,6 @@ double _add(double a, double b);
 double _sub(double a, double b);
 double _mul(double a, double b);
 double _div(double a, double b);
+
+
+int car(int x);  // ajout de la fonction carré
